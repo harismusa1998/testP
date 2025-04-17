cbd8ed23ae76789a9d5365005e933628922f2f31
harismusa1998 <106408561+harismusa1998@users.noreply.github.com>
2025-04-17 09:43:23 +0200

    Add files via upload

---
+++ testP/utils/testData.js
@@ -0,0 +1,16 @@
+module.exports = {
+    validUser: {
+      username: 'Testuser123456',
+      email: 'harriis98@gmail.com',
+      password: 'StrongPassword123!'
+    },
+    invalidEmailUser: {
+      email: 'invalidemail'
+    },
+    existingEmailUser: {
+      username: 'Testuser121212',
+      email: 'harrii.boss@gmail.com',
+      password: 'Testuser123!'
+    }
+  }
+  
